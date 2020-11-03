3570f70acb5f3826a9bcfcd1d702d840e4796269
中島祐樹 <nakajima@jsl.co.jp>
2020-11-03 11:49:35 +0900

    wip

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import datas from './assets/フィロソフィーのダンス/ダンス・オア・ダンス/ダンス・オア・ダンス.json'
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['data', 'options'],
+
+  mounted () {
+    this.renderChart({
+      labels: [...datas.bars].map(value => value.start),
+      datasets: [
+        {
+          label: 'Data',
+          backgroundColor: '#f87979',
+           borderDash: [5, 5],
+          borderWidth: 1,
+          data: [...datas.bars].map(value => value.confidence) 
+        }
+      ]
+    })
+  }
+}
+</script>
+<style>
+</style>
